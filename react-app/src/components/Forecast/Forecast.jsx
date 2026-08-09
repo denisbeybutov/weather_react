12a6805ddd9d2629b313938f9fd46c8b3d057b45
denisbeybutov <beybutov.denis@mail.ru>
2026-08-09 15:33:08 +0300

    почасовой доделал

--- react-app/src/components/Forecast/Forecast.jsx
+++ react-app/src/components/Forecast/Forecast.jsx
@@ -1,23 +1,51 @@
+
+import { weather } from '../Card/Card'
+import { getWeatherIcon } from '../Icons/Icons'
 import './Forecast.css'
+
+function formatHour(time){
+    return new Date(time).toLocaleTimeString('ru-RU',{
+        hour: '2-digit',
+        minute: '2-digit'
+    })
+}
+
+function getCurrentIndex(times) {
+    const now = new Date()
+
+    return times.findIndex(time => {
+        const date = new Date(time)
+
+        return (
+            date.getFullYear() === now.getFullYear() &&
+            date.getMonth() === now.getMonth() &&
+            date.getDate() === now.getDate() &&
+            date.getHours() === now.getHours()
+        )
+    })
+}
 
 export default function Forecast({city}){
     if(!city) {
         return null
     }
 
+    const currentIndex = getCurrentIndex(city.hourlyTime)
+
     return (
         <section className="forecast">
             Почасовой прогноз
             <div className="forecast_items">
-                <div>время 
-                    {city.hourlyTime.slice(0,24).map((time,index )=> 
+                
+                    {city.hourlyTime.slice(currentIndex,currentIndex+12).map((time,index )=> 
                         <div>
-                            <div>{time}</div>
-                            <div>{city.hourlyTemperature[index]}</div>
+                            <div>{formatHour(time)}</div>
+                            <div>{Math.round(city.hourlyTemperature[index])}°C</div>
+                            <img src={getWeatherIcon(city.hourlyWeatherCode[index])} alt="" style={{width:'30px'}} />
                         </div>
                        
                     )}
-                </div>
+                
                 
             </div>
         </section>
